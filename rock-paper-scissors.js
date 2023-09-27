66ab38f9afc67266e41b173adbdaf0dfbadb7f28
Harshal Patil <harshalspatil21@gmail.com>
2023-09-27 16:49:22 +0530

    added keyboard functionlity

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -39,6 +39,17 @@ document.querySelector('.js-paper-button').addEventListener('click',()=>{
 document.querySelector('.js-scissors-button').addEventListener('click',()=>{
     playGame('Scissors');
 })
+
+document.body.addEventListener('keydown',(event)=>{
+    if(event.key==='r'){
+        playGame('Rock');
+    }else if(event.key==='p'){
+        playGame('Paper');
+    }else if(event.key==='s'){
+        playGame('Scissors');
+    }
+});
+
 function playGame(userMove){
     const computerMove=pickComputerMove();
     let result='';
